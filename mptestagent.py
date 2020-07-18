4ecdc138cc1efa710b142c1ae60dbe4f49a58b11
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-07-18 10:46:47 +0900

    extreme

--- mptestagent.py
+++ mptestagent.py
@@ -128,7 +128,7 @@ class Player():
     def brain_layers(self, x):
         x = layers.Flatten()(x)
         x = layers.Dense(256, activation='relu')(x)
-        x = layers.Dense(1024, activation='relu')(x)
+        x = layers.Dense(8192, activation='relu')(x)
         x = layers.Dense(1024, activation='relu')(x)
         x = layers.Dense(1024, activation='relu')(x)
         x = layers.Dense(512, activation='relu')(x)
